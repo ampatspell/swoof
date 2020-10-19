735b330c80346c0c2a9ed9530cc4e0a974c7c311
Arnis Vuskans <ampatspell@gmail.com>
2020-10-19 09:36:51 +0300

    dummy

--- dummy/src/Query.svelte
+++ dummy/src/Query.svelte
@@ -6,6 +6,13 @@
   let query = store.collection('messages').query();
   setGlobal({ query });
 
+  let add = async () => {
+    let next = query.content.length + 1;
+    await query.ref.doc().new({
+      text: `Document #${next}`
+    }).save();
+  }
+
 </script>
 
 <div class="query">
@@ -20,12 +27,17 @@
   {/await}
 </div>
 
+<div class="actions">
+  <button on:click={add}>Add</button>
+</div>
+
 <div class="docs">
   {#each $query.content as doc}
     <div class="doc">
       <JSON object={doc}/>
       <input bind:value={doc.data.text}/>
       <button on:click={() => doc.save()}>Save</button>
+      <button on:click={() => doc.delete()}>Delete</button>
     </div>
   {/each}
 </div>
@@ -35,6 +47,9 @@
     margin-bottom: 10px;
   }
   .status {
+    margin-bottom: 10px;
+  }
+  .actions {
     margin-bottom: 10px;
   }
   .docs {
