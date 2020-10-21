06d4a136bb753e16b6d7a59e0ec3f4e93e845816
Arnis Vuskans <ampatspell@gmail.com>
2020-10-21 17:39:16 +0300

    make state a store

--- dummy/src/Blank.svelte
+++ dummy/src/Blank.svelte
@@ -1,3 +1,9 @@
 <script>
-
+  import { state } from 'swoof';
 </script>
+
+<div class="snapshots">
+  {#each $state.snapshots as snapshot}
+    <div>{snapshot}</div>
+  {/each}
+</div>
--- src/state.js
+++ src/state.js
@@ -1,20 +1,32 @@
 import { toString } from './util';
+import { writable } from 'svelte/store';
 
 class State {
 
   constructor() {
     this._snapshots = new Set();
+    this._writable = writable(this);
   }
 
   get snapshots() {
     return [ ...this._snapshots ];
   }
 
+  _notify() {
+    this._writable.set(this);
+  }
+
   registerOnSnapshot(model) {
     this._snapshots.add(model);
+    this._notify();
     return () => {
       this._snapshots.delete(model);
+      this._notify();
     }
+  }
+
+  subscribe() {
+    return this._writable.subscribe(...arguments);
   }
 
   toString() {
