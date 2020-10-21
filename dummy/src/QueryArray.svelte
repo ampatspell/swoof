8aaa15d6571afb18e521a3e037d5772d71ef37b6
Arnis Vuskans <ampatspell@gmail.com>
2020-10-22 01:19:50 +0300

    dummy

--- dummy/src/QueryArray.svelte
+++ dummy/src/QueryArray.svelte
@@ -14,6 +14,10 @@
       this.property('query', attr(store.collection('messages').query()));
     }
 
+    async add() {
+      await store.collection('messages').doc().new({ name: 'new one' }).save();
+    }
+
     get serialized() {
       let { query } = this;
       return {
@@ -29,11 +33,47 @@
 
 </script>
 
+{#each $model.query.content as doc}
+  <div class="doc">
+    <div class="row">
+      <div class="key">id</div>
+      <div class="value">{doc.id}</div>
+    </div>
+    <div class="row">
+      <div class="key">name</div>
+      <div class="value">
+        <input bind:value={doc.data.name}/>
+        <button on:click={() => doc.save()}>Save</button>
+        <button on:click={() => doc.delete()}>Delete</button>
+      </div>
+    </div>
+  </div>
+{/each}
+
+<div class="row">
+  <button on:click={() => $model.add()}>Add new</button>
+</div>
+
 <div class="row">
   <JSON object={$model}/>
 </div>
 
 <style type="text/scss">
+  .doc {
+    margin-bottom: 15px;
+    > .row {
+      display: flex;
+      flex-direction: row;
+      align-items: baseline;
+      > .key {
+        min-width: 50px;
+        font-size: 13px;
+      }
+      > .value {
+        flex: 1;
+      }
+    }
+  }
   .row {
     margin-bottom: 5px;
   }
