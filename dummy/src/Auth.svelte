aa70c4c8800f0d725bfa4f0e28571dd0cff5811d
Arnis Vuskans <ampatspell@gmail.com>
2020-10-22 20:32:24 +0300

    dummy

--- dummy/src/Auth.svelte
+++ dummy/src/Auth.svelte
@@ -27,14 +27,43 @@
   let model = writable(new Auth());
   setGlobal({ model: model.value });
 
+  let email;
+  let password;
+
 </script>
 
+{#await $model.auth.promise}
+  Loading…
+{/await}
+
 <div class="row">
-  <JSON object={$model}/>
+  <div class="label">Anonymous</div>
+  <input type="button" value="Sign in" on:click={() => $model.auth.methods.anonymous.signIn()}/>
 </div>
 
-<style>
+<div class="row">
+  <div class="label">Email</div>
+  <input placeholder="email" bind:value={email}/>
+  <input placeholder="password" bind:value={password}/>
+  <input type="button" value="Sign in" on:click={() => $model.auth.methods.email.signIn(email, password)}/>
+  <input type="button" value="Sign up" on:click={() => $model.auth.methods.email.signUp(email, password)}/>
+</div>
+
+<div class="row">
+  <input type="button" value="Sign out" on:click={() => $model.auth.signOut()}/>
+  <input type="button" value="Delete" disabled={!$model.auth.user} on:click={() => $model.auth.user.delete()}/>
+</div>
+
+<div class="row">
+  <JSON object={$model.auth.user}/>
+</div>
+
+<style type="text/scss">
   .row {
-    margin-bottom: 5px;
+    > .label {
+      font-size: 13px;
+      margin-bottom: 5px;
+    }
+    margin-bottom: 15px;
   }
 </style>
